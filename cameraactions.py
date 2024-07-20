62fec4a73d69d597c5feb8122a1110832febae41
M.Hamza SEZER <88151573+mhamzasezer@users.noreply.github.com>
2024-07-20 22:17:57 +0900

    Update cameraactions.py

--- cameraactions.py
+++ cameraactions.py
@@ -7,9 +7,9 @@ from reolinkapi import Camera
 
 
 # Replace with your camera's IP, username, and password
-camera_ip = '192.168.0.52'
+camera_ip = ''
 username = 'admin'
-password = '6540pachinko'
+password = ''
 
 # Initialize the camera
 camera = Camera(ip=camera_ip, username=username, password=password)
